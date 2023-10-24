c49d026d8dc6e2de2d36247df1a96a7ad56d2afa
AnderIS2 <ajromero@jdc.edu.co>
2023-10-24 13:35:28 -0500

    :tada: Funciona Template MaterialPro

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# StoreApp
+# MaterialProAngularLite
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.7.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.1.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'StoreApp'`, () => {
+  it(`should have as title 'material-pro-angular-lite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('StoreApp');
+    expect(app.title).toEqual('material-pro-angular-lite');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('StoreApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('material-pro-angular-lite app is running!');
   });
 });
